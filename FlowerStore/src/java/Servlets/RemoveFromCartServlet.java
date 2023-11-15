d498f1e29b91831bb7159a5929a8cbb691cbf882
EvacATL <98920892+EvacATL@users.noreply.github.com>
2023-11-14 20:21:17 -0500

    Added Remove From Cart
    Created remove from cart servlet
    Linked servlet to remove from cart button

---
+++ FlowerStore/src/java/Servlets/RemoveFromCartServlet.java
@@ -0,0 +1,69 @@
+package Servlets;
+
+import Business.*;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jose V Gomez
+ */
+@WebServlet(name = "RemoveFromCartServlet", urlPatterns = {"/remove-from-cart"})
+public class RemoveFromCartServlet extends HttpServlet {
+
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter()) {
+            String productCode = request.getParameter("productCode");
+            if(productCode != null){
+                ArrayList<Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list");
+                if(cart_list != null){
+                    for(Cart c:cart_list){
+                        if(c.getProductCode().equals(productCode)){
+                            cart_list.remove(cart_list.indexOf(c));
+                            break;
+                        }
+                    }
+                    response.sendRedirect("cart.jsp");
+                }
+            }else{
+                response.sendRedirect("cart.jsp");
+            }
+        }
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
